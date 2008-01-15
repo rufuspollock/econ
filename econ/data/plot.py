7af1904c1253f437937cc1c2e0d5aa36f09af15a
rgrp <rufus.pollock@okfn.org>
2008-01-15 20:13:03 +0000

    [plot]: improved ploticus wrapper.
      * Substantially improved docstrings.
      * Add supported for kwargs for all plot functions and moved this functionality into common method.
      * Added verbose option to __init__.
      * Minor general refactoring.

--- econ/data/plot.py
+++ econ/data/plot.py
@@ -86,18 +86,48 @@ class Ploticus(object):
     '''Ploticus based plotting.
 
     @cvar out_format: The output format to use (svg/png/x11)
+    @cvar ploticus: name of ploticus binary to call.
+
+    Most functions take an extra kwargs variable. Important possible parameters
+    are:
+        * header='yes' (data has a header)
+        * title=... (title for plot)
+        * More details on
+          <http://ploticus.sourceforge.net/doc/prefab_stdparms.html>
+
+    Demo usage:
+
+    >>> from econ.data.plot import Ploticus
+    >>> pl = Ploticus()
+    >>> x = [1,2,3]
+    >>> y = [4,5,6]
+    >>> graph_data = pl.line(x,y, title='My Plot')
+    >>> fo = file('mygraph.png', 'wb')
+    >>> fo.write(graph_data)
+    >>> fo.close()
     '''
     out_format = 'png'
     ploticus = 'pl'
+    colors = 'red orange green purple yellow blue magenta tan1 coral tan2 claret pink brightgreen brightblue limegreen yellowgreen lavender powderblue redorange lightorange'.split(' ')
+    '''Array with all the colors in it'''
 
-    def line(self, x, y):
+    def __init__(self, verbose=False):
+        '''
+        @arg verbose: be verbose (e.g. print command string)
+        '''
+        self.verbose = verbose
+
+    def line(self, x, y, **kwargs):
         '''Plot a line chart.
 
-        @arg x: x values
-        @ arg y: y values
+        @arg x: A list of x values.
+        @arg y: a list of y values.
+        @arg **kwargs: additional key/value options to be passed to ploticus.
+        @return: graph image data (binary).
         '''
         # in_file_path, out_file_path, **kwargs):
         # self.cmd = '-prefab lines data=%(input)s x=1 y=14 y2=7 y3=8 y4=18 delim=comma header=yes' 
+        self.kwargs = kwargs
         self.cmd = ' -prefab lines x=1 y=2 delim=csv '
         self.input = self._make_csv(x,y)
         return self._run()
@@ -105,19 +135,18 @@ class Ploticus(object):
     def pie(self, values, lables, **kwargs):
         '''Plot a pie chart.
 
-        @arg lables: A list of labels
-        @arg values: A list of y values to use
-        @arg opts: Options
+        @arg values: A list of y values to use.
+        @arg lables: A list of labels.
+        @arg **kwargs: additional key/value options to be passed to ploticus.
+        @return: graph image data (binary).
         '''
-        #Array with all the colors in it:
-        colors = 'red orange green purple yellow blue magenta tan1 coral tan2 claret pink brightgreen brightblue limegreen yellowgreen lavender powderblue redorange lightorange'.split(' ')
+        self.kwargs = kwargs
 
-        cmd = ' -prefab pie data=stdin labels=1 values=2 colorfld=3 delim=csv '
-        options = [ '%s=%s' % (k,v) for k,v in kwargs.items() ]
-        self.cmd = cmd + ' '.join(options)
+        cmd = ' -prefab pie labels=1 values=2 colorfld=3 delim=csv '
         self.input = ''
-        while len(colors) < len(values):
-            colors += colors
+        colors = self.colors
+        while len() < len(values):
+            colors += self.colors
         for i,j,k in zip(lables,values,colors):
             self.input += '%s,%s,%s\n' % (i,j,k)
         return self._run()
@@ -125,12 +154,13 @@ class Ploticus(object):
     def vbar(self, x, y, **kwargs):
         '''Plot a (vertical) bar chart.
 
-        @arg x: A list of x values to use (can be names)
-        @arg y: A list of y values to use (must be numeric)
+        @arg x: A list of x values to use (can be names).
+        @arg y: A list of y values to use (must be numeric).
+        @arg **kwargs: additional key/value options to be passed to ploticus.
+        @return: graph image data (binary).
         '''
-        cmd = ' -prefab vbars x=1 y=2 delim=csv '
-        options = [ '%s=%r' % (k,v) for k,v in kwargs.items() ]
-        self.cmd = cmd + ' '.join(options)
+        self.kwargs = kwargs
+        self.cmd = ' -prefab vbars x=1 y=2 delim=csv '
         self.input = self._make_csv(x,y)
         return self._run()
 
@@ -139,14 +169,24 @@ class Ploticus(object):
         for x,y in zip(xvals, yvals):
             out += '%s, %s\n' % (x,y)
         return out
+    
+    def _make_cmd(self):
+        options = [ '%s=%s' % (k,v) for k,v in self.kwargs.items() ]
+        self.cmd = self.cmd + ' '.join(options)
+        fullcmd = '%s -%s -data=stdin -o stdout %s ' % (self.ploticus, self.out_format,
+            self.cmd)
+        return fullcmd
 
     def _run(self):
         import popen2
-        fullcmd = '%s -%s -data=stdin -o stdout %s ' % (self.ploticus, self.out_format,
-            self.cmd)
+        fullcmd = self._make_cmd()
+        if self.verbose:
+            print fullcmd
         cstdout, cstdin, cstderr = popen2.popen3(fullcmd)
         cstdin.write(self.input)
         cstdin.close()
         data = cstdout.read()
+        if self.verbose:
+            print cstderr.read()
         return data
 
