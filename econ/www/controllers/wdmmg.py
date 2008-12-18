49587becafe5df1aac69ce1e67c5ef692715cf98
rgrp <rufus.pollock@okfn.org>
2008-12-18 18:29:03 +0000

    [plot,js,wdmmg][m]: select all/select none support for plots and improve wdmmg go charting demos.

--- econ/www/controllers/wdmmg.py
+++ econ/www/controllers/wdmmg.py
@@ -1,6 +1,9 @@
+import genshi
+
 from econ.www.lib.base import *
 
 import econdata.ukgovfinances.data as d
+import econ.data
 
 class WdmmgController(BaseController):
 
@@ -8,19 +11,42 @@ class WdmmgController(BaseController):
         return render('wdmmg/index')
 
     def spend(self):
+        a = d.Analyzer()
         entries, years = a.extract_simple()
         expenditure = entries['Public sector current expenditure']
-        d1 = [ map(lambda x: [x[0], x[1]], zip(years, expenditure)) ]
-        c.datasets = [ d1 ]
-        c.fig_title = 'UK Government Expenditure'
+        import econ.data
+        td = econ.data.TabularData(
+                header=['Year'],
+                data=[years]
+                )
+        for k,v in entries.items():
+            td.header.append(k)
+            td.data.append(v)
+        td.data = econ.data.transpose(td.data)
+
+        import econ.www.controllers.plot
+        plotctr = econ.www.controllers.plot.PlotController()
+        c.html_table = genshi.XML(plotctr.get_html_table(td))
+        c.chart_code = genshi.HTML(plotctr._get_chart_code(td))
+        c.fig_title = u'UK Government Expenditure (Millions)'
         return render('wdmmg/spend')
 
     def dept(self):
         a = d.Analyzer()
         data = a.extract_dept_spend()
-        d1 = [ data[k] for k in data ]
-        c.datasets = [ [d1] ]
-        c.type = 'pie'
+        # create bar positions at 1,2,3,...
+        count = 1
+        c.datasets = []
+        for k in data:
+            c.datasets.append(
+                {'label': k,
+                 'data': [[ count, data[k] ]]
+                 }
+                )
+            count += 1
+        import simplejson as sj
+        # won't plot more than 5 or so items with bars ...
+        c.datasets = sj.dumps(c.datasets[:30])
         c.fig_title = 'UK Government Spending by Dept (2007)'
         return render('wdmmg/dept')
 
