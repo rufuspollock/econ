60043ba4694a43a46b528f7bc685bf17220f39b8
rgrp <rufus.pollock@okfn.org>
2009-02-04 17:36:45 +0000

    [econ][xs]: fix up out of date get_config code to use simple pylons approach (this also stops modpython breaking!).

--- econ/__init__.py
+++ econ/__init__.py
@@ -5,26 +5,17 @@ __long_description__ = \
 '''
 '''
 
-def setup_config():
-    conf = get_config()
-    CONFIG.push_process_config({'app_conf': conf.local_conf,
-                                'global_conf': conf.global_conf}) 
-
-def get_config(default_path='development.ini'):
-    """Load config file and return configobj containing that information.
-    
-    Config file is located using 'ECONCONF' environment variable or, if that
-    does not exist, looking for a file 'development.ini' (note that this path
-    is taken in relation to the current directory.
-    """
-    import os
-    default_path = os.path.abspath(default_path)
-    env_var_name = __application_name__.upper() + 'CONF'
-    config_file = os.environ.get(env_var_name, default_path)
-    if not os.path.exists(config_file):
-        raise ValueError('No Configuration file exists at: %s' % config_file)
-    # do it this round-about way so as to go through paste
-    import paste.deploy
-    conf = paste.deploy.appconfig('config:' + config_file)
+def get_config():
+    from pylons import config
+    conf = config
     return conf
+
+def register_config(config_path):
+    import os
+    config_path = os.path.abspath(config_path)
+    import paste.deploy
+    pasteconf = paste.deploy.appconfig('config:' + config_path)
+    import econ.config.environment
+    econ.config.environment.load_environment(pasteconf.global_conf,
+        pasteconf.local_conf)
 
